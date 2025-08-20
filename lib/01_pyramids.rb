8afe3cc3b2925157fd874c17222d1682c64d86c4
ibm-webmaster <mahamoudisb@gmail.com>
2025-08-20 13:17:28 +0100

    Ajout de la methode full_pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,7 +1,7 @@
 #exo_02.rb de la pyramide encapsulé dans la méthode half_pyramid
 
 def half_pyramid
-  puts "Salut, bienvenue dans ma super pyramide !"
+  puts "Salut, bienvenue dans ma super demi-pyramide !"
   puts "Combien d'étages veux-tu ? (entre 1 et 25)"
   print "> "
   nombre = gets.chomp.to_i
@@ -19,3 +19,23 @@ def half_pyramid
 end
 
 half_pyramid
+
+def full_pyramid
+  puts "Salut, bienvenue dans ma super pyramide complet !"
+  puts "Combien d'étages veux-tu ? (entre 1 et 25)"
+  print "> "
+  nombre = gets.chomp.to_i
+
+  if nombre < 1 || nombre > 25
+    puts "Erreur : choisis un nombre entre 1 et 25."
+  else
+    puts "Voici la pyramide complète :"
+    1.upto(nombre) do |i|
+      espaces = " " * (nombre - i)
+      diese   = "#" * (2 * i - 1)
+      puts espaces + diese
+    end
+  end
+end
+
+full_pyramid
